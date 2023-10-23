92211b2d1c1a203120881a2294decbeaf4a86dbd
sina.kadkhodaee <sina.kadkhodaee@partsoftware.com>
2023-10-23 17:17:52 +0330

    Implement four direction moving ability

--- go.mod
+++ go.mod
@@ -4,13 +4,7 @@ go 1.21.3
 
 require (
 	atomicgo.dev/cursor v0.2.0
-	github.com/inancgumus/screen v0.0.0-20190314163918-06e984b86ed3
 	github.com/nsf/termbox-go v1.1.1
 )
 
-require (
-	github.com/mattn/go-runewidth v0.0.9 // indirect
-	golang.org/x/crypto v0.14.0 // indirect
-	golang.org/x/sys v0.13.0 // indirect
-	golang.org/x/term v0.13.0 // indirect
-)
+require github.com/mattn/go-runewidth v0.0.9 // indirect
--- index.go
+++ index.go
@@ -3,8 +3,6 @@ package main
 import (
 	"fmt"
 
-	"atomicgo.dev/cursor"
-	"github.com/inancgumus/screen"
 	term "github.com/nsf/termbox-go"
 )
 
@@ -13,28 +11,30 @@ var animal = "|"
 type Animal struct {
 	X     int
 	Y     int
-	Shape string
+	Shape rune
 }
 
-var snake = Animal{X: 0, Y: 0, Shape: "●"}
+var snake = Animal{X: 0, Y: 0, Shape: '●'}
 
 func main() {
-	var width, height = screen.Size()
-	snake.X = width / 2
-	snake.Y = height / 2
+	term.Init()
+	term.HideCursor()
+	defer term.Close()
 
 	startGame()
 }
 
 func startGame() {
-	term.Init()
-	term.HideCursor()
-	defer term.Close()
+	var width, height = term.Size()
+	snake.X = width / 2
+	snake.Y = height / 2
 
 Infinite:
 	for {
-		cursor.Move(snake.X, snake.Y)
-		fmt.Print(snake.Shape)
+		term.Clear(term.ColorDefault, term.ColorDefault)
+		term.SetChar(snake.X, snake.Y, snake.Shape)
+		term.Sync()
+		fmt.Print(" ", snake.X, snake.Y)
 
 		var event = term.PollEvent()
 
@@ -42,20 +42,56 @@ Infinite:
 			switch event.Key {
 			case term.KeyArrowLeft:
 				snake.moveLeft()
+			case term.KeyArrowRight:
+				snake.moveRight()
+			case term.KeyArrowUp:
+				snake.moveUp()
+			case term.KeyArrowDown:
+				snake.moveDown()
 			case term.KeyCtrlC:
 				break Infinite
 			}
-
-			term.Sync()
-			fmt.Print(snake.X, snake.Y)
 		}
 	}
 }
 
 func (animal *Animal) moveLeft() {
-	if animal.X > 0 {
+	if animal.isNotOnBoundry('l') {
 		animal.X -= 1
-	} else {
-		animal.X = 0
 	}
+}
+
+func (animal *Animal) moveRight() {
+	if animal.isNotOnBoundry('r') {
+		animal.X += 1
+	}
+}
+
+func (animal *Animal) moveUp() {
+	if animal.isNotOnBoundry('u') {
+		animal.Y -= 1
+	}
+}
+
+func (animal *Animal) moveDown() {
+	if animal.isNotOnBoundry('d') {
+		animal.Y += 1
+	}
+}
+
+func (animal *Animal) isNotOnBoundry(direction rune) bool {
+	var width, height = term.Size()
+
+	switch direction {
+	case 'l':
+		return animal.X > 0
+	case 'r':
+		return animal.X < width
+	case 'u':
+		return animal.Y > 0
+	case 'd':
+		return animal.Y < height
+	}
+
+	panic("Bad direction")
 }
