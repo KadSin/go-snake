cca86b5286e8cab88987f2bd396ca8b4bfeda1e0
sina.kadkhodaee <sina.kadkhodaee@partsoftware.com>
2023-11-06 15:20:53 +0330

    Implement the shooter Entity

---
+++ entities/shooter.go
@@ -0,0 +1,37 @@
+package entities
+
+import (
+	"time"
+
+	term "github.com/nsf/termbox-go"
+)
+
+type Shooter struct {
+	Person     Object
+	Bullet     Object
+	IsShooting bool
+}
+
+func (shooter *Shooter) Shoot() {
+	shooter.IsShooting = true
+
+	shooter.Bullet.Direction = shooter.Person.Direction
+	shooter.Bullet.X = shooter.Person.X
+	shooter.Bullet.Y = shooter.Person.Y
+	shooter.Bullet.UpdateLocation(2)
+
+	ticker := time.NewTicker(time.Second / 20)
+
+	for range ticker.C {
+		term.SetChar(shooter.Bullet.X, shooter.Bullet.Y, ' ')
+
+		error := shooter.Bullet.UpdateLocation(1)
+		if error != nil {
+			shooter.IsShooting = false
+			break
+		}
+
+		term.SetCell(shooter.Bullet.X, shooter.Bullet.Y, shooter.Bullet.Shape, term.ColorLightYellow, term.ColorDefault)
+		term.Sync()
+	}
+}
