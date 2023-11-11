04be91c70c0fbb33dac6806c1be5c681b7cbaa34
sina.kadkhodaee <sina.kadkhodaee@partsoftware.com>
2023-11-11 12:07:41 +0330

    Test the location changing of object

--- go.mod
+++ go.mod
@@ -2,6 +2,15 @@ module kadsin/shoot-run
 
 go 1.21.3
 
-require github.com/nsf/termbox-go v1.1.1
+require (
+	github.com/nsf/termbox-go v1.1.1
+	github.com/stretchr/testify v1.8.4
+)
 
-require github.com/mattn/go-runewidth v0.0.9 // indirect
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/mattn/go-runewidth v0.0.9 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.5.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- tests/object_test.go
+++ tests/object_test.go
@@ -1,7 +1,47 @@
 package tests
 
-import "testing"
+import (
+	"kadsin/shoot-run/entities"
+	"testing"
 
-func TestUpdateLocation(t *testing.T) {
-	t.Skip()
+	"github.com/stretchr/testify/assert"
+)
+
+var obj entities.Object
+
+func TestUpdateLocationToUp(t *testing.T) {
+	obj.MoveUp()
+	UpdateLocation()
+
+	assert.Equal(t, 5, obj.Y)
+}
+
+func TestUpdateLocationToRight(t *testing.T) {
+	obj.MoveRight()
+	UpdateLocation()
+
+	assert.Equal(t, 15, obj.X)
+}
+
+func TestUpdateLocationToDown(t *testing.T) {
+	obj.MoveDown()
+	UpdateLocation()
+
+	assert.Equal(t, 15, obj.Y)
+}
+
+func TestUpdateLocationToLeft(t *testing.T) {
+	obj.MoveLeft()
+	UpdateLocation()
+
+	assert.Equal(t, 5, obj.X)
+}
+
+func UpdateLocation() {
+	entities.TerminalSize = func() (int, int) { return 20, 20 }
+
+	obj.X = 10
+	obj.Y = 10
+
+	obj.UpdateLocation(5)
 }
