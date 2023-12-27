46b1b491d946a0d203e912bfa8dcac34b888f6e3
sina.kadkhodaee <sina.kadkhodaee@partsoftware.com>
2023-12-27 14:15:01 +0330

    Make more variants for enemy chasing

--- game/entities/enemy.go
+++ game/entities/enemy.go
@@ -1,5 +1,7 @@
 package entities
 
+import "math/rand"
+
 type Enemy struct {
 	Person Object
 	Target *Object
@@ -8,17 +10,34 @@ type Enemy struct {
 }
 
 func (enemy *Enemy) Chase() {
-	if enemy.Target.Location.X > enemy.Person.Location.X {
-		enemy.Person.MoveRight()
-	} else if enemy.Target.Location.X < enemy.Person.Location.X {
-		enemy.Person.MoveLeft()
-	}
-	enemy.Person.UpdateLocation(1)
+	canChaseTwoDirections := rand.Float32() > 0.5
+	canChaseVertical := rand.Float32() > 0.5
 
+	if canChaseVertical || canChaseTwoDirections {
+		enemy.chaseVertical()
+	}
+
+	if !canChaseVertical || canChaseTwoDirections {
+		enemy.chaseHorizontal()
+	}
+}
+
+func (enemy *Enemy) chaseVertical() {
 	if enemy.Target.Location.Y > enemy.Person.Location.Y {
 		enemy.Person.MoveDown()
 	} else if enemy.Target.Location.Y < enemy.Person.Location.Y {
 		enemy.Person.MoveUp()
 	}
+
+	enemy.Person.UpdateLocation(1)
+}
+
+func (enemy *Enemy) chaseHorizontal() {
+	if enemy.Target.Location.X > enemy.Person.Location.X {
+		enemy.Person.MoveRight()
+	} else if enemy.Target.Location.X < enemy.Person.Location.X {
+		enemy.Person.MoveLeft()
+	}
+
 	enemy.Person.UpdateLocation(1)
 }
